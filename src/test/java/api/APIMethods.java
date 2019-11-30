978f0e2be91fba1a49c3de387c8816be7158af9b
trugaaa <andrey.trugaaa@gmail.com>
2019-11-30 22:13:43 +0200

    Property files adding Jersey work starting

--- src/test/java/api/APIMethods.java
+++ src/test/java/api/APIMethods.java
@@ -1,25 +1,45 @@
 package api;
 
 import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
-public class APIMethods {
-    private static final String ROOT_URL = "https://ccostsproject.azurewebsites.net";
+import java.io.FileInputStream;
+import java.util.Properties;
 
+public class APIMethods {
+    private static  String rootURL;
     private static String validAuthToken;
     private static final String INVALID_AUTH_TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9u" +
             "YW1lIjoiQWRtaW4iLCJuYmYiOjE1NzQ4ODE3NTMsImV4cCI6MTU3NDg4MjM1MywiaXNzIjoiQ0NUZWFtIiwiYXVkIjoiQ0NVc2VycyJ9.yz1mP4VMxBzN516mvmH-T0F9VshYQmVfbnmzi6CkOw";
 
     //TODO CREate normal work
     /*
-    Client client;
-    WebResource webResource;
+
     public static ClientResponse settingThingsUp(){
         webResource=client.resource(APIMethods.getURLWithEndpoint("/login"));
         ClientResponse response = webResource.accept("application/json").post(ClientResponse.class);
         //assertEquals(response.getStatus(),400);
     }*/
+    public APIMethods()
+    {
+        FileInputStream fis;
+        Properties property = new Properties();
+        Client client;
+        WebResource webResource;
+
+
+
+        try {
+            fis = new FileInputStream("src/test/api/resources/configAPI.properties");
+            property.load(fis);
+
+            rootURL = property.getProperty("rootURL");
+        }
+        catch (Exception ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+    }
 
     public static String getValidAuthToken() {
         return validAuthToken;
@@ -35,6 +55,6 @@ public class APIMethods {
 
     public static String getURLWithEndpoint(String endpoint)
     {
-        return ROOT_URL + endpoint;
+        return rootURL + endpoint;
     }
 }
